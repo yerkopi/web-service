9c06cd1dad785f36041721ff1a09084b551e636c
machinetherapist <79029454+illegal-instruction-co@users.noreply.github.com>
2023-08-17 17:36:06 +0300

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -6,11 +6,11 @@ source "$DIR/../.env"
 
 git config --global --add safe.directory "$SERVICE_DIR"
 
-systemctl disable "$SERVICE_NAME"
-systemctl stop "$SERVICE_NAME"
+sudo systemctl disable "$SERVICE_NAME"
+sudo systemctl stop "$SERVICE_NAME"
 
 cp -f "$SERVICE_DIR/$SERVICE_NAME" "/lib/systemd/system/$SERVICE_NAME"
 
-systemctl daemon-reload
-systemctl enable "$SERVICE_NAME"
-systemctl start "$SERVICE_NAME"
+sudo systemctl daemon-reload
+sudo systemctl enable "$SERVICE_NAME"
+sudo systemctl start "$SERVICE_NAME"
